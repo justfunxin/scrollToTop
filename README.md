1007cb53ed02f7e5b82033671310863d6972e58c
kangarooxin <pangxin001@163.com>
2016-04-21 00:59:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# scrollToTop
+
+### Useage:
+
+```javascript
+$('.scrollToTop').scrollToTop();
+```
+### Options:
+
+```javascript
+$.fn.scrollToTop.defaults = {
+    container: window,
+    target : 'html, body',
+    speed : 800,
+    distance: 0,
+    top: 0
+}
+```
+### Customized with metadata
+
+```html
+<a href="#" class="scrollToTop" data-scroll-target="#div1" data-scroll-speed="600" data-scroll-distance="100" data-scroll-top="10">TOP</a>
+```
---
+++ jquery.scrollToTop.js
@@ -0,0 +1,55 @@
+(function($) {
+    $.fn.scrollToTop = function(options) {
+        if(options && options.target && !options.container) {
+            options.container = options.target;
+        }
+        options = $.extend({}, $.fn.scrollToTop.defaults, options);
+        return this.each(function() {
+            var $this = $(this);
+            var dataOptions = getDataOptions($this);
+            if(dataOptions.target && !dataOptions.container) {
+                dataOptions.container = dataOptions.target;
+            }
+            var opts = $.extend({}, options, dataOptions);
+            var $container = $(opts.container);
+            if($container.scrollTop() > opts.distance) {
+                $this.show();
+            } else {
+                $this.hide();
+            }
+            $container.scroll(function() {
+                if ($container.scrollTop() > opts.scrollDistance) {
+                    $this.fadeIn();
+                } else {
+                    $this.fadeOut();
+                }
+            });
+            $this.click(function(e) {
+                e.preventDefault();
+                $(opts.target).animate({
+                    scrollTop : opts.top
+                }, opts.speed);
+            });
+        });
+    };
+
+    function getDataOptions(element) {
+        var opt, value,
+            options = {}, $element = $(element);
+        for (opt in $.fn.scrollToTop.defaults) {
+            value = $element.data("scroll" + opt.charAt(0).toUpperCase() + opt.substring(1).toLowerCase());
+            if (value !== undefined) {
+                options[opt] = value;
+            }
+        }
+        return options;
+    }
+
+    $.fn.scrollToTop.defaults = {
+        container: window,
+        target : 'html, body',
+        speed : 800,
+        distance: 0,
+        top: 0
+    }
+})(jQuery);
